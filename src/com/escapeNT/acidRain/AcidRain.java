f557bbe886972bba0195ed0d663874b7e4de11e4
escapeNT <auxadress@gmail.com>
2011-07-20 18:35:21 -0400

    Implemented command system

--- src/com/escapeNT/acidRain/AcidRain.java
+++ src/com/escapeNT/acidRain/AcidRain.java
@@ -2,7 +2,10 @@
 
 package com.escapeNT.acidRain;
 
+import org.bukkit.ChatColor;
 import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event.Priority;
 import org.bukkit.event.Event.Type;
@@ -22,6 +25,7 @@ public class AcidRain extends JavaPlugin {
     public void onEnable() {
         Util.setPlugin(this);
 
+        // Config
         if(!Config.settingsFile.exists()) {
             Config.create();
         }
@@ -31,6 +35,7 @@ public class AcidRain extends JavaPlugin {
             Util.getWorldIsAcidRaining().put(w, Boolean.FALSE);
         }
 
+        // Register events
         PluginManager pm = this.getServer().getPluginManager();
         pm.registerEvent(Type.WEATHER_CHANGE, new AcidRainWeatherListener(), Priority.Monitor, this);
         pm.registerEvent(Type.PLAYER_MOVE, new AcidRainPlayerListener(), Priority.Monitor, this);
@@ -57,6 +62,51 @@ public class AcidRain extends JavaPlugin {
     public void onDisable() {
         Util.log("disabled.");
     }
+    
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
+	if(cmd.getName().equalsIgnoreCase("Acidrain")) {
+            World w;
+            switch(args.length) {
+                case 0:
+                    if(!(sender instanceof Player)) {
+                        sender.sendMessage(ChatColor.RED + "You must specify a world if you aren't logged in!");
+                        return false;
+                    }
+                    w = ((Player)sender).getWorld();
+                    w.setStorm(true);
+                    w.setWeatherDuration(12000);
+                    Util.getWorldIsAcidRaining().put(w, Boolean.TRUE);
+                    break;
+                case 1:
+                    try {
+                        w = getServer().getWorld(args[0]);
+                    } catch(Exception ex) {
+                        sender.sendMessage(ChatColor.RED + "World not found!");
+                        return false;
+                    }
+                    w.setStorm(true);
+                    w.setWeatherDuration(12000);
+                    Util.getWorldIsAcidRaining().put(w, Boolean.TRUE);
+                    break;
+                case 2:
+                    try {
+                        w = getServer().getWorld(args[0]);
+                        w.setStorm(true);
+                        w.setWeatherDuration(Integer.parseInt(args[1]));
+                    } catch(NumberFormatException ex) {
+                        sender.sendMessage(ChatColor.RED + "Duration is not a number!");
+                        return false;
+                    } catch(Exception ex) {
+                        sender.sendMessage(ChatColor.RED + "World not found!");
+                        return false;
+                    }
+                    Util.getWorldIsAcidRaining().put(w, Boolean.TRUE);
+            }
+            return true;
+	}
+	return false; 
+    }
 
     // API ----------------------------------------------------------
 
@@ -66,11 +116,11 @@ public class AcidRain extends JavaPlugin {
      * @return True if the world currently has acid rain.
      */
     public boolean isAcidRaining(World world) {
-        if(!Util.getWorldIsAcidRaining().containsKey(world)) {
-            return false;
+        if(Util.getWorldIsAcidRaining().containsKey(world)) {
+            return Util.getWorldIsAcidRaining().get(world);
         }
         else {
-            return Util.getWorldIsAcidRaining().get(world);
+            return false;
         }
     }
 }
